b31186e6e46973f80360dc938434b2de241b2ce9
sdaitzman <samuel@daitzman.com>
2016-06-08 10:28:33 -0400

    progress

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 #!/usr/bin/env node
-let fs = require('fs');
-let SerialPort = require("serialport").SerialPort
-let serialPort = new SerialPort("//file location", {
-  baudrate: 250000
-});
+const fs = require('fs')
+const SerialPort = require("serialport")
+const _ = require('lodash')
+const every = require('every-time-mirror')
 
-SendGcode("teensy");
+SerialPort.list((err, ports) => {
+  let printerPort = ports[0].comName
+  _.forEach(ports, (port) => {
+    if(~_.lowerCase(port.manufacturer).indexOf('ultimachine')) printerPort = port.comName
+  })
+  let printer = new SerialPort.SerialPort(printerPort, {
+    baudrate: 115200,
+    parser: SerialPort.parsers.readline('\n')
+  })
 
-function SendGcode(file) {
-  let gcodefile = +file + ".gcode";
+  printer.on('open', () => {
+    printer.on('data', (data) => {
+      if(data === 'start') {
+        sendGcode('teensy')
+      }
+    })
+  })
+})
+
+
+
+// let serialPort = new SerialPort("//file location", {
+//   baudrate: 250000
+// });
+
+function sendGcode(file) {
+  let gcodefile = file + ".gcode";
   let lineReader = require('readline').createInterface({
     input: require('fs').createReadStream(gcodefile)
   })
-
-  console.log(line)
-
 }

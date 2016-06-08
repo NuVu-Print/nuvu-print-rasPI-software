5cbe9c82f4a9bb3d55f6f2b0911e39645342207a
Louie Adamian <louieadamian@gmail.com>
2016-06-07 20:43:08 -0400

    uses serialport

--- app.js
+++ app.js
@@ -1,43 +1,39 @@
-ar fs = require('fs');
-var state = {
-    distmode: true, // true = abs, false = rel
-    extruderTemp: 215,
-    bedTemp: 85,
-    x: 0,
-    y: 0,
-    z: 0,
-    xMin: 000,
-    xMax: 400,
-    yMin: 000,
-    yMax: 500,
-    zMin: 000,
-    zMax: 800,
-    extruderSteps: 0
-}
-var location = -1
-var elapsedtime = 0;
-var speedXY = 500;
-var speedZ = 100;
-var speedXY = 50;
-speedXY = speedXY / 1
-speedZ = speedZ / 1
-console.log(speedXY);
-console.log(speedZ);
+  var fs = require('fs');
+  var SerialPort = require("serialport").SerialPort
+  var serialPort = new SerialPort("//file location",
+  baudrate: 250000
+  });
+  var state = {
+      distmode: true, // true = abs, false = rel
+      extruderTemp: 215,
+      bedTemp: 85,
+      x: 0,
+      y: 0,
+      z: 0,
+      xMin: 000,
+      xMax: 400,
+      yMin: 000,
+      yMax: 500,
+      zMin: 000,
+      zMax: 800,
+      extruderSteps: 0;
+  }
+  var sent = false;
+  var location = -1;
+  var elapsedtime = 0;
 
-SendGcode()
+  SendGcode("teensy");
 
-function SendGcode(file) {
-    var gcodefile = "gcode/" + file + ".gcode";
-    var lineReader = require('readline').createInterface({
-        input: require('fs').createReadStream(gcodefile)
-    });
+  function SendGcode(file) {
+      var gcodefile = +file + ".gcode";
+      var lineReader = require('readline').createInterface({
+          input: require('fs').createReadStream(gcodefile)
+      })
 
-    elapsedtime = 0;
-    lineReader.on('line', function(line) {
-        var tokens = line.split(" ");
-        // console.log(tokens);
-        parseGcode(tokens);
-        resToken();
-    });
-    console.log(tokens);
-}
+      elapsedtime = 0;
+      lineReader.on('line', function(line) {
+          if (sent) {
+              console.log(line);
+          }
+      })
+  }
---
+++ readme.md
@@ -0,0 +1,1 @@
+#G-code File Serving

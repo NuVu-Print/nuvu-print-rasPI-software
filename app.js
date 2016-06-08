af4837aafd46bf2fde47b0d11e7abfbd3b24d3f7
sdaitzman <samuel@daitzman.com>
2016-06-08 09:33:28 -0400

    changes

--- app.js
+++ app.js
@@ -1,39 +1,18 @@
-  var fs = require('fs');
-  var SerialPort = require("serialport").SerialPort
-  var serialPort = new SerialPort("//file location",
+#!/usr/bin/env node
+let fs = require('fs');
+let SerialPort = require("serialport").SerialPort
+let serialPort = new SerialPort("//file location", {
   baudrate: 250000
-  });
-  var state = {
-      distmode: true, // true = abs, false = rel
-      extruderTemp: 215,
-      bedTemp: 85,
-      x: 0,
-      y: 0,
-      z: 0,
-      xMin: 000,
-      xMax: 400,
-      yMin: 000,
-      yMax: 500,
-      zMin: 000,
-      zMax: 800,
-      extruderSteps: 0;
-  }
-  var sent = false;
-  var location = -1;
-  var elapsedtime = 0;
+});
 
-  SendGcode("teensy");
+SendGcode("teensy");
 
-  function SendGcode(file) {
-      var gcodefile = +file + ".gcode";
-      var lineReader = require('readline').createInterface({
-          input: require('fs').createReadStream(gcodefile)
-      })
+function SendGcode(file) {
+  let gcodefile = +file + ".gcode";
+  let lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream(gcodefile)
+  })
 
-      elapsedtime = 0;
-      lineReader.on('line', function(line) {
-          if (sent) {
-              console.log(line);
-          }
-      })
-  }
+  console.log(line)
+
+}

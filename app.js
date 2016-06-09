f261ea6769d6a25e0492a8023168a6df880af9f7
sdaitzman <samuel@daitzman.com>
2016-06-09 11:04:11 -0400

    accept arbitrary filename as argument
    for printing tests today

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const every = require('every-time-mirror')
 
 let lines = []
 let lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream('reed.gcode')
+  input: require('fs').createReadStream(process.argv[2])
 })
 lineReader.on('line', (line) => {
   if(line.charAt(0) !== ';' && line) {
